a06f37320ed0c09fd5f260bf0cbf7bb6b03de0ec
mohdfaizan29 <mofaizan2910204@gmail.com>
2025-08-11 22:54:09 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition {
+    public static void main(String[] args){
+        int a = 155;
+        int b = 158;
+       float c = a/b;
+       System.out.println("Sum of a and b : "+c);
+    }
+    
+}
