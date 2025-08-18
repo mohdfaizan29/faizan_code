8a9384d5a52bdb3bdb35570cab317153ccba5c90
mohdfaizan29 <mofaizan2910204@gmail.com>
2025-08-18 23:24:14 +0530

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,26 @@
+public class Grade {
+    public static void main(String[] args){
+        int marks = 100;
+        if(marks>80){
+            System.out.println("Grade = A");
+    
+        }
+        else if(marks>70){
+            System.out.println("Grade = B");
+        }
+        else if(marks>60){
+            System.out.println("Grade = C");
+        }
+        else if(marks>50){
+            System.out.println("Grade = D");
+        }
+        else if(marks<50){
+            System.out.println("Grade = E");
+        }
+
+
+
+
+    }
+    
+}
