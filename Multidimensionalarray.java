a36793635517a5466a88f45061634c44e83d2161
mohdfaizan29 <mofaizan2910204@gmail.com>
2025-08-26 23:36:10 +0530

    Add files via upload

---
+++ Multidimensionalarray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class Multidimensionalarray {
+    public static void main(String[] args){
+        int[][] numbers = {{1,2,3},{2,3,4}};
+        System.out.println(Arrays.deepToString(numbers));
+
+
+    }
+}
