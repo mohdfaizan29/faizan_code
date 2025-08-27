5b56642dae955cee4f9f384922ac6ad325844634
mohdfaizan29 <mofaizan2910204@gmail.com>
2025-08-27 22:44:43 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        byte age = sc.nextByte();
+        System.out.println("Age = " +age);
+
+    }
+}
