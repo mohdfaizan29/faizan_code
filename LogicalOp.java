8ac4e9c902420968533625787766c1dd8900cd42
mohdfaizan29 <mofaizan2910204@gmail.com>
2025-08-27 22:46:07 +0530

    Add files via upload

---
+++ LogicalOp.java
@@ -0,0 +1,7 @@
+public class LogicalOp {
+    public static void main(String[] args){
+        int temperature = 22;
+        boolean isWarm = temperature>20 && temperature<30;
+        System.out.println(isWarm);
+    }
+}
