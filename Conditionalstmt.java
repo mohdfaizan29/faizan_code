7982c4647606d175d32eeda1746febea9bdc0fd6
mohdfaizan29 <mofaizan2910204@gmail.com>
2025-08-28 23:16:31 +0530

    Add files via upload

---
+++ Conditionalstmt.java
@@ -0,0 +1,20 @@
+public class Conditionalstmt {
+    public static void main(String[] args){
+        int temp = 32;
+
+        if(temp>30){
+            System.out.println("Its hot day");
+            System.out.println("Drink water");
+ 
+        } 
+        else if (temp>20){
+            System.out.println("Its beautiful day");
+        }
+        else {
+            System.out.println("Its cold");
+        }
+
+        }
+
+    }
+
